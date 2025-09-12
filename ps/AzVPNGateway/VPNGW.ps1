5d36c0048b0b813a71fe3828e334ab6e149a457e
yamapan <71251920+aktsmm@users.noreply.github.com>
2025-09-12 17:24:05 +0900

    Revise comments for VPN Gateway SKU configurations
    Revise comments for VPN Gateway SKU configurations

--- ps/AzVPNGateway/VPNGW.ps1
+++ ps/AzVPNGateway/VPNGW.ps1
@@ -3,7 +3,11 @@
 $Ctime=Get-Date -Format "hhmmss"
 
 # Set required paramerters (Please change the parameters according to your environment)
-## こちらはVpnGw1 or VpnGw1AZ SKU ＋ Basic Public IP の構成はVpnGw1 or VpnGw1AZ SKU が2025/09/ 現在Basic Public IP に対応していないため使えないです。
+## 2025/09/ 現在 VpnGw1 or VpnGw1AZ SKU ＋ Basic Public IP の構成は VpnGw1 or VpnGw1AZ SKU が Basic Public IP に対応していないため使えないです。
+## ✅ Basic VPN GW + Basic PIP (Dynamic)     → 作成可（9月30日まで）
+## ❌ Basic VPN GW + Standard PIP (Static)   → 作成不可(そのうち対応？)
+## ❌VpnGw1-5Az VPN GW + Basic PIP (Dynamic)   → 作成不可
+## ✅ VpnGw1-5Az VPN GW + Standard PIP (Static) → 作成可（今後メイン商用VPNGW)
 $GatewaySku = "Basic" ## Basic or VpnGw1 or VpnGw1AZ　
 $resourceGroupName = "rg-"+$GatewaySku +$Ctime
 $location = "japaneast"
